5eeaf1617fe692740d591ff67a76308bb9f84cc3
hbhandari0005 <hbhandari0005@gmail.com>
2025-03-18 17:53:10 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -56,6 +56,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.get('/',(req,res){
+  res.send("Server working")
+})
+
 app.post("/signup", async (req, res) => {
   try {
     let { username, password } = req.body;
