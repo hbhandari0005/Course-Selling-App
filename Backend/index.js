9720ae789f3dc02bd3229de9ce2760a366f13c4b
hbhandari0005 <hbhandari0005@gmail.com>
2025-03-18 22:18:56 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -237,4 +237,8 @@ app.post("/:courseId/edit", upload.single("image"), async (req, res) => {
 const port=process.env.PORT || 3000
 app.listen(port, () => {
   console.log("Server connected on port 3000");
+})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
