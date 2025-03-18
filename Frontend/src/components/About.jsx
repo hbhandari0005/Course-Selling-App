77df07d56db1b2023f86562fbd642526cdd0d9a3
hbhandari0005 <hbhandari0005@gmail.com>
2025-03-19 00:27:40 +0530

    Update About.jsx

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -1,121 +1,84 @@
 import React from "react";
-
-function About() {
+const About = () => {
   return (
-    <>
-      <div className="about d-flex flex-column align-items-center justify-content-center  bg-dark text-white text-center p-4 ">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam quis
-        impedit aspernatur asperiores beatae earum dignissimos nobis pariatur
-        placeat sapiente voluptatibus minus, ipsum similique eaque alias
-        nesciunt rerum perspiciatis itaque delectus veniam cons ectetur. Cumque
-        vitae tenetur praesentium quaerat. A eos adipisci amet molliti a nostrum
-        minus, soluta perspiciatis inventore laborum distinctio unde quas qui a
-        laboriosam corrupti exercitationem reiciendis expedita fuga? Ratione
-        perspici atis possimus provident eligendi dolorum optio cum blanditiis,
-        recusandae, quo autem doloribus ab fugit quas ipsum, quibusdam maxime
-        perferendis nesciunt accusamus architecto corporis maiores consequuntur.
-        Veniam, culpa molestiae. Facilis, quis! Inventore vel recusandae,
-        reiciendis odit officiis doloribus deserunt eius, rem voluptatem,
-        eveniet voluptatum eaque alias soluta voluptatibus a magni earum cumque
-        exercitationem? Obcaecati, ratione delectus aliquam dolores at optio
-        magni. Tenetur nisi veniam in facere illum temporibus ducimus officiis
-        nihil ratione. Rem at dolore nihil assumenda iure ipsum aspernatur,
-        iusto in ullam id, vero ea totam accusantium? Explicabo saepe
-        perferendis tempora quaerat quidem porro rem qui alias dignissimos,
-        facere quis deleniti. Nulla assumenda nostrum omnis quis! Distinctio
-        esse deserunt voluptates illo perspiciatis harum ex, ipsam officiis
-        vero, et blanditiis molestias. Sit, eius voluptas quas debitis
-        doloremque doloribus modi similique voluptatem cum excepturi aliquam
-        blanditiis animi sint repellendus minus repudiandae earum eligendi quis
-        voluptatum velit culpa, deleniti natus libero dolores? Laboriosam modi
-        voluptatibus, eaque, officiis doloribus alias recusandae quod fugit quas
-        dolorum iusto quibusdam voluptate incidunt maiores voluptatem
-        repudiandae nesciunt inventore architecto dolore blanditiis beatae
-        laudantium exercitationem quis aut. Voluptates modi cumque doloremque
-        voluptas odio cupiditate omnis mollitia ducimus, eius qui neque veniam
-        officia. Numquam, nulla ea ad ullam non perferendis itaque autem,
-        adipisci unde beatae quia enim quis eaque cumque ab vero! Alias tempora
-        unde sint et numquam? Corrupti maxime voluptas vel quod? Eligendi
-        recusandae soluta sunt rerum impedit aspernatur dolor facilis reiciendis
-        necessitatibus laudantium, quaerat, rem quibusdam esse amet minus quae
-        eos aliquid porro minima asperiores nemo ipsum. Quis optio explicabo
-        consequatur velit eius dolorem alias perferendis, laboriosam amet,
-        debitis facere fugiat. Dicta, perferendis modi mollitia libero excepturi
-        facilis quam, quos itaque autem dolore explicabo amet! Tempore alias
-        soluta deleniti incidunt nesciunt excepturi, modi veniam temporibus
-        necessitatibus eum ullam obcaecati explicabo perspiciatis officia
-        doloremque, provident cumque debitis enim quia. Repellat exercitationem
-        voluptate magni unde corrupti, odit sapiente amet, beatae quibusdam
-        possimus est at nobis sequi magnam ea! Illo tempore, nulla sequi porro
-        repudiandae laudantium dolorum officia unde obcaecati sint doloremque,
-        necessitatibus maiores consequuntur cum perspiciatis ea earum. Tenetur
-        odio officiis dolor qui, earum harum non, porro libero sint dicta
-        deleniti sapiente eveniet eligendi iste inventore laudantium quia. Modi
-        perspiciatis, dolorem repellat itaque porro molestiae quos molestias
-        possimus, aperiam, fugit exercitationem assumenda. Deleniti culpa est
-        vero perspiciatis, fugit ipsum numquam voluptas magni iste autem iusto,
-        reprehenderit dolore vitae veritatis. Odio a itaque cumque dolorem eum
-        fuga distinctio sit modi cum similique atque esse obcaecati, deleniti in
-        veniam quisquam voluptatum autem laboriosam. Harum nesciunt, deserunt
-        veritatis obcaecati expedita iure! Officia accusantium distinctio iusto
-        sunt quod rem provident culpa consequuntur, veniam vero maxime voluptas
-        fuga nam odit autem aut sequi minima rerum, repellendus necessitatibus
-        facilis excepturi quo? Quia ducimus commodi cum porro nostrum natus
-        praesentium voluptas, inventore iusto maiores ex cumque deleniti dolorum
-        sequi maxime nihil quas nemo sint iure quisquam obcaecati impedit? Saepe
-        rem id voluptatem magni soluta! Ad incidunt cumque, facilis, inventore
-        iste veritatis ex praesentium fugit at ratione illo velit, laboriosam
-        itaque sunt dolor repudiandae. Est repellat perferendis, sunt saepe, quo
-        numquam quis tenetur in illo architecto omnis dicta dolorum fugit eum?
-        Nobis non quo labore ipsa ad? Nesciunt nemo enim iure, amet tenetur
-        debitis dolor accusantium qui culpa esse fugiat temporibus minima omnis,
-        modi dolores voluptate doloremque cum suscipit, voluptas earum corporis
-        perferendis. Vero ad nisi exercitationem quae iusto provident! Earum
-        sequi, ipsa cupiditate optio recusandae commodi, similique, minima rem
-        deleniti cumque voluptate dolore eaque assumenda a odit consequatur
-        soluta blanditiis? Excepturi veniam repellendus quod error nostrum, illo
-        quo vitae, explicabo cumque ducimus natus fuga omnis nihil a eos
-        delectus, autem quaerat ad veritatis? Consequuntur quibusdam quae dolor
-        distinctio minus iste dolore nesciunt magni id, est quos sunt ut
-        repudiandae doloribus ipsa! Fugit ipsum doloremque perferendis et nulla
-        praesentium officia, iste minima temporibus quaerat iure cupiditate
-        iusto autem id nobis dolorem rem maxime nam quo ducimus molestias
-        facilis provident! Eaque repudiandae quidem pariatur, voluptatum tempore
-        mollitia earum magnam iusto asperiores provident eos itaque rem dicta
-        possimus? Non saepe necessitatibus earum quod aperiam quidem, rem
-        asperiores error distinctio consectetur rerum natus qui beatae ex
-        molestiae dolorum amet aut quisquam. Ipsum corporis tempore maxime
-        dolor, minus ducimus accusantium? Cupiditate ea repellat ab pariatur sit
-        ullam optio veritatis ad! Ut quaerat consectetur explicabo natus
-        sapiente necessitatibus eligendi fugit libero vitae deserunt. Quas
-        consectetur, vel, consequuntur dolorem ex nemo suscipit iusto qui, sunt
-        possimus blanditiis corporis autem exercitationem? Ipsam pariatur minima
-        tempora illo ipsum delectus, sunt dicta quidem et, minus aliquam maxime
-        eius nisi nam dolorem ex porro, libero eligendi? Iste quaerat laborum
-        delectus ipsa consequatur aspernatur laudantium iure necessitatibus
-        accusantium, dolore, ipsam magni cum corrupti quod asperiores suscipit
-        quibusdam laboriosam quis, ipsum ratione ab. Sapiente ex, quae debitis
-        quas officiis asperiores neque atque suscipit nesciunt minima delectus
-        quod in nisi esse excepturi voluptatum voluptas at ratione sit
-        temporibus iste. Quisquam reiciendis nobis recusandae fugit doloribus!
-        Culpa odit officiis commodi alias nulla! Eveniet, beatae? Incidunt,
-        autem, harum voluptatum obcaecati veniam dolores modi temporibus
-        exercitationem ullam totam odit quo atque unde reiciendis dicta
-        blanditiis culpa esse. Reprehenderit, saepe. Commodi delectus aut qui
-        provident voluptates quae dolores labore, deserunt explicabo eveniet rem
-        veniam cupiditate ipsam perspiciatis id autem dicta est dolorem
-        consequuntur harum nemo repellendus nesciunt aliquam. Non, similique
-        enim. Sunt modi nisi corporis nemo tempore porro deleniti commodi
-        libero, impedit, odit eveniet id voluptatem, corrupti aperiam adipisci
-        ea? Id unde, culpa modi itaque alias ver o nobis quia nihil eum dolore
-        doloribus at vel! Impedit pariatur minima dolores aperiam repellat quod
-        sapiente? Autem laudantium veritatis molestias, eveniet cumque explicabo
-        quae animi vero iusto numquam ad eligendi non minus doloremque
-        exercitationem fugiat soluta qui consequatur mollitia dolores
-        consectetur vel. Nulla quam placeat ratione saepe vel.
+    <div className="about bg-dark py-5">
+      <div className="container about">
+        <div className="text-center mb-5 about">
+          <h1 className="text-warning fw-bold about">About CourseBazaar</h1>
+          <p className="lead text-white about"><strong>Your Gateway to Quality Education</strong></p>
+        </div>
+
+        <div className="text-center mb-5 about">
+          <p className="fs-5 text-white about">
+            Welcome to <strong className="about">CourseBazaar</strong>, a platform designed to help learners discover and access high-quality courses. Whether you're a student, a professional, or an enthusiast looking to upskill, CourseBazaar provides a seamless experience to explore educational resources and enhance your knowledge.
+          </p>
+        </div>
+
+        <div className="row align-items-center bg-secondary shadow p-4 rounded mb-5 about">
+          <div className="col-md-12 about">
+            <h2 className="text-warning about">Our Mission</h2>
+            <p className="text-white about">At <strong className="about">CourseBazaar</strong>, we believe that education should be:</p>
+            <ul className="list-group list-group-flush text-white about">
+              <li className="list-group-item text-white bg-secondary border-0 about"><strong>Accessible:</strong> Everyone deserves to learn.</li>
+              <li className="list-group-item text-white bg-secondary border-0 about"><strong>Organized:</strong> Finding courses should be easy.</li>
+              <li className="list-group-item text-white bg-secondary border-0 about"><strong>Empowering:</strong> Education should help individuals grow.</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row align-items-center bg-secondary shadow p-4 rounded mb-5 about">
+          <div className="col-md-12 about">
+            <h2 className="text-warning about">Key Features</h2>
+            <ul className="list-group list-group-flush text-white about">
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Extensive Course Library:</strong> Courses in various disciplines.</li>
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Easy Course Navigation:</strong> Smart search for quick access.</li>
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Curated & Trusted Content:</strong> High-quality courses from top educators.</li>
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Personalized Learning Experience:</strong> Recommendations based on your goals.</li>
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Regular Updates:</strong> Always up-to-date educational resources.</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row align-items-center bg-secondary shadow p-4 rounded mb-5 about">
+          <div className="col-md-12 about">
+            <h2 className="text-warning about">How CourseBazaar Works</h2>
+            <ol className="text-white about">
+              <li className="about"><strong>Search & Explore:</strong> Browse courses in different categories.</li>
+              <li className="about"><strong>Compare & Choose:</strong> Get course details, reviews, and pricing.</li>
+              <li className="about"><strong>Enroll & Learn:</strong> Pick a course and start learning at your own pace.</li>
+            </ol>
+          </div>
+        </div>
+
+        <div className="row align-items-center bg-secondary shadow p-4 rounded mb-5 about">
+          <div className="col-md-12 about">
+            <h2 className="text-warning about">Why Choose CourseBazaar?</h2>
+            <ul className="list-group list-group-flush text-white about">
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Comprehensive & Diverse:</strong> Courses for all learning levels.</li>
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Time-Saving & Efficient:</strong> Everything in one place.</li>
+              <li className="text-white list-group-item bg-secondary border-0 about"><strong>Trusted by Learners:</strong> A growing community of students and professionals.</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row align-items-center bg-secondary shadow p-4 rounded mb-5 about">
+          <div className="col-md-12 about">
+            <h2 className="text-warning about">Our Vision</h2>
+            <p className="text-white about">
+              We envision a future where education is accessible to all, empowering individuals with the knowledge and skills they need to succeed. CourseBazaar aims to become a leading platform that connects learners with top-quality courses, making lifelong learning a reality for everyone.
+            </p>
+          </div>
+        </div>
+
+        <div className="text-center about">
+          <h2 className="text-warning about">Join Us</h2>
+          <p className="text-white about">
+            Start your learning journey with <strong className="about">CourseBazaar</strong> today. Discover new courses, gain valuable skills, and take a step towards a brighter future.
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default About;
