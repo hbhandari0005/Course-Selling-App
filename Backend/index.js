62fec45bcd233b84eeb023067cbc17dd22055bb1
hbhandari0005 <hbhandari0005@gmail.com>
2025-03-20 14:14:23 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://harshit-course-selling-mern.vercel.app/",
+    origin: "https://harshit-course-selling-mern.vercel.app",
     credentials: true, 
     methods: "GET,POST,PUT,DELETE",
   })
