cfc951c3825963ca66525e85ea3bcca3ea2eef1a
hbhandari0005 <hbhandari0005@gmail.com>
2025-03-20 13:13:19 +0530

    Update CreateCourse.jsx

--- Frontend/src/components/CreateCourse.jsx
+++ Frontend/src/components/CreateCourse.jsx
@@ -27,7 +27,7 @@ function CreateCourse({ setRefresh }) {
       formData.append("image", image); 
       setSubmitDone(true)
       const response = await axios.post(
-        `https://harshit-course-selling-mern.onrender.com/${userAtLocalStorage._id}/createCourse`,
+        `https://full-stack-jet-omega.vercel.app/${userAtLocalStorage._id}/createCourse`,
           formData,{
             headers: {
               "Content-Type": "multipart/form-data"
